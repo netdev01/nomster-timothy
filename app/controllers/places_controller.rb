3634bae2a6f13e31ca9a74b86b6b15dda2b830c8
Timothy <tim@gotomyhome.com>
2017-05-31 04:22:11 +0000

    30

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -2,7 +2,7 @@ class PlacesController < ApplicationController
 	before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
 	def index
-		@places = Place.page(params[:page]).per(1)
+		@places = Place.page(params[:page]).per(3)
 	end
 
 	def new

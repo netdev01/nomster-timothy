4a1b448bdfefd0a4384386b6da5a18d6272a59f0
Timothy <tim@gotomyhome.com>
2017-06-01 18:33:22 +0000

    34

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,8 +5,20 @@ class CommentsController < ApplicationController
 	def create
 		@place = Place.find(params[:place_id])
 		@place.comments.create(comment_params.merge(user: current_user))
+
+=begin
+# alternative 1
+Comment.create(comment_params.merge( :user => current_user, :place_id => params[:id] )
+# alternative 2
+@place.comments.new(comment_params)
+@place.user = current_user
+@place.save
+=end
+
 		redirect_to place_path(@place)
 	end
+
+	private
 
 	def comment_params
 		params.require(:comment).permit(:message, :rating)

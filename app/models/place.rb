4777af73e49d5973ebc1b0187d0f65e32d4d6e2d
Timothy <tim@gotomyhome.com>
2017-06-01 16:07:40 +0000

    31

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,5 +1,7 @@
 class Place < ApplicationRecord
 	belongs_to :user
+	has_many :comments
+
 	validates :name, :address, :description, presence: true
 	validates :name, length: { minimum: 3 }
 	geocoded_by :address

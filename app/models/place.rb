9377cab214237371d918d2d256d25e506ff5bcc2
netdev01 <tim@gotomyhome.com>
2017-06-04 12:04:38 +0000

    39 take 2

--- app/models/place.rb
+++ app/models/place.rb
@@ -3,6 +3,8 @@ class Place < ApplicationRecord
 	has_many :comments
 	has_many :photos
 
+  mount_uploader :picture, PictureUploader
+
 	validates :name, :address, :description, presence: true
 	validates :name, length: { minimum: 3 }
 	geocoded_by :address

ff2e325d0ed47eb82a379b3cfe508fa1791444be
netdev01 <tim@gotomyhome.com>
2017-06-04 11:51:23 +0000

    39 initial

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,6 +1,7 @@
 class Place < ApplicationRecord
 	belongs_to :user
 	has_many :comments
+	has_many :photos
 
 	validates :name, :address, :description, presence: true
 	validates :name, length: { minimum: 3 }

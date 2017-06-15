9176c03616fbe30e30454afe814cebea4316d156
netdev01 <tim@gotomyhome.com>
2017-06-15 15:15:14 +0000

    bug fix

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -6,6 +6,17 @@ class PhotosController < ApplicationController
 		redirect_to place_path(@place)
 	end
 
+	def destroy
+		@place = Place.find(params[:place_id])
+		if @place.user != current_user
+			return render text: 'Not Allowed', status: :forbidden
+		end
+		@id = params[:id]
+		@photo = Photo.find(@id)
+		@photo.destroy if @place == @photo.place
+		redirect_to place_path(@place)
+	end
+
 	private
 
 	def photo_params
